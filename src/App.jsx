8736e1faa165bb1d88f99fb13fcb5536f46b539f
Nathan Geronimo <nathangero@yahoo.com>
2023-11-30 17:30:19 -0800

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+    
+    return (
+        <footer>
+            Footer
+            {/* TODO: Put github, linkedin, and medium logos with links */}
+        </footer>
+    )
+}
