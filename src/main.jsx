82aaea395bac775c790fd3209b12a5180a5f8eb3
Nathan Geronimo <nathangero@yahoo.com>
2023-11-30 17:28:54 -0800

    add basic Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
+
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import AboutMePage from './pages/AboutMePage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: '/aboutme',
+        element: <AboutMePage />
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage />
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 )
