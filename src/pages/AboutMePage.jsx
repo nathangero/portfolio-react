eba4bcef171f3437958f9089da47deddffa1aee8
Nathan Geronimo <nathangero@yahoo.com>
2023-12-01 15:47:30 -0800

    comment out app.css

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,10 +1,27 @@
 
 
 export default function AboutMePage() {
-    
+
+    const styles = {
+        background: {
+            "backgroundColor": "red",
+            "border": "none"
+        }
+    }
+
     return (
-        <div>
-            About me
-        </div>
+        <section className="w-100 m-0" style={styles.background}>
+            <p>
+                I'm a full stack iOS Developer based in the Bay Area. My specialties are in SwiftUI, Google Cloud Platform, Google Firebase, and NodeJS. I created and currently maintain iOS rideshare apps called Fijo Rideshare & Delivery and Fijo Driver. Currently, I'm learning web development through the UC Berkeley Extension course and hope to make a careers switch afterwards.
+            </p>
+            <br />
+            <p>
+                In my freetime I like to try different food places around the Bay Area with my loving partner, hangout with my friends whether its through Discord or IRL, travel to see family, participate in my church activities/worship throughout the week, and improving my coding skills through personal projects or LeetCode/HackerRank practice.
+            </p>
+            <br />
+            <p>
+                Please check out my work and contact info below, and thank you for visiting!
+            </p>
+        </section>
     )
 }
