62507cfe839d96a514f9047af7960d453d26411e
Nathan Geronimo <nathangero@yahoo.com>
2023-11-30 19:17:29 -0800

    implement bootstrap navbar class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,36 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className='navbar navbar-expand-lg'>
-            navbar
+            <div className='container-fluid'>
+                <div className='collapse navbar-collapse'>
+                    <div className='navbar-nav'>
+                        <li>
+                            <Link
+                            className='nav-link'
+                            to={'/'}
+                            >
+                            About Me
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                            className='nav-link'
+                            to={'/portfolio'}
+                            >
+                            Portfolio
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                            className='nav-link'
+                            to={'/contact'}
+                            >
+                            Contact
+                            </Link>
+                        </li>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
