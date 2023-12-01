9c5f1bbde57b5b031313f467911a58fa213a8cee
Nathan Geronimo <nathangero@yahoo.com>
2023-11-30 20:41:30 -0800

    implement setting active nav tab

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# portfolio-react
+# My Portfolio (React)
+
+## Description
+
+
+
+## Image
+
+
+
+## Learning Points
+
+
+
+## Code Snippets
+
+
+## Credits 
+
+[Bootstrap align navbar to right](https://www.studytonight.com/bootstrap/how-to-align-bootstrap-5-navbar-items-to-the-right)
+
+
+## Resources
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,69 @@
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const NAV_LINKS = {
+        ABOUT_ME: "aboutme",
+        PORTFOLIO: "portfolio",
+        CONTACT: "contact"
+    }
+
+    const onClickLink = (link) => {
+        switch (link) {
+            case NAV_LINKS.ABOUT_ME:
+                document.getElementById('nav-aboutme').className = "nav-link active";
+                document.getElementById('nav-portfolio').className = "nav-link";
+                document.getElementById('nav-contact').className = "nav-link";
+                break;
+
+            case NAV_LINKS.PORTFOLIO:
+                document.getElementById('nav-aboutme').className = "nav-link";
+                document.getElementById('nav-portfolio').className = "nav-link active";
+                document.getElementById('nav-contact').className = "nav-link";
+                break;
+
+            case NAV_LINKS.CONTACT:
+                document.getElementById('nav-aboutme').className = "nav-link";
+                document.getElementById('nav-portfolio').className = "nav-link";
+                document.getElementById('nav-contact').className = "nav-link active";
+                break;
+        }
+    }
+
     return (
-        <nav className='navbar navbar-expand-lg'>
+        <nav className='navbar navbar-expand-sm fixed-top '>
             <div className='container-fluid'>
-                <div className='collapse navbar-collapse'>
-                    <div className='navbar-nav'>
-                        <li>
-                            <Link
+                <div className='collapse navbar-collapse justify-content-end'>
+                    <div className='navbar-nav nav-underline'>
+                        <Link
+                            id='nav-aboutme'
                             className='nav-link'
                             to={'/'}
-                            >
+                            onClick={() => onClickLink(NAV_LINKS.ABOUT_ME)}
+                        >
                             About Me
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
+                        </Link>
+                        <Link
+                            id='nav-portfolio'
                             className='nav-link'
                             to={'/portfolio'}
-                            >
+                            onClick={() => onClickLink(NAV_LINKS.PORTFOLIO)}
+                        >
                             Portfolio
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
+                        </Link>
+                        <Link
+                            id='nav-contact'
                             className='nav-link'
                             to={'/contact'}
-                            >
-                            Contact
-                            </Link>
-                        </li>
+                            onClick={() => onClickLink(NAV_LINKS.CONTACT)}
+                        >
+                            Contact Me
+                        </Link>
+                        <Link
+                            className='nav-link'
+                            to={'/contact'} // TODO: Download the resume
+                        >
+                            Resume
+                        </Link>
                     </div>
                 </div>
             </div>
