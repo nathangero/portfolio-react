410687b635d2e4e4fd2154afeacdf3ce163a457e
Nathan Geronimo <nathangero@yahoo.com>
2023-12-01 23:41:44 -0800

    implement project data, ProjectCard, ProjectPage

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,21 @@
 
 
 export default function ProjectCard(props) {
-    
+
+    const project = props.project;
+
     return (
-        <div>
-            ProjectCard
+        <div className="card w-25">
+            <img src={project.image} className="card-img-top" alt={project.alt} />
+            <div className="card-body">
+                <h5 className="card-title text-center">{project.name}</h5>
+                <p className="card-text text-center">{project.description}</p>
+
+                <div className="d-flex justify-content-evenly">
+                    <a target="_blank" href={project.deploy_link} className="btn btn-primary">Website</a>
+                    <a target="_blank" href={project.repo_link} className="btn btn-primary">Code Repo</a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,10 +1,14 @@
-
+import ProjectData from "/src/assets/projects.json";
+import ProjectCard from '../components/ProjectCard.jsx'
 
 export default function ProjectPage() {
-    
+    const projects = ProjectData.map((project, index) => <li key={index}><ProjectCard project={project} /></li> )
+
     return (
         <section>
-            Projects
+            <h2 className="text-center">Here&apos;s a list of projects I&apos;m proud of!</h2>
+
+            <ul className="list-unstyled">{projects}</ul>
         </section>
     )
 }
