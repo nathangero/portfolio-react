e7d6912d72a0accfea3fffc95196672715bd47f6
Nathan Geronimo <nathangero@yahoo.com>
2023-12-01 21:31:41 -0800

    implementing resume download link

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,9 +2,23 @@
 
 export default function ResumePage() {
     
+
+    const onClickDownloadResume = () => {
+        console.log("@onClickDownloadResume")
+
+        const resumePath = "src/assets/nathan_resume.pdf";
+        const tempLink = document.createElement("a"); // Create a clickable element
+        tempLink.href = resumePath;
+        tempLink.download = "Nathan's Resume.pdf"; // Set name for file
+
+        document.body.appendChild(tempLink);
+        tempLink.click(); // Activate the download link
+        document.body.removeChild(tempLink); // Remove the element
+    };
+
     return (
         <div>
-            resume
+            <button className="btn btn-primary" type="button" onClick={ onClickDownloadResume }>Download My Resume</button>
 
             list of skills
         </div>
