1f5da8408c7f75012e72fd2a2113c38ad9f4a090
Nathan Geronimo <nathangero@yahoo.com>
2023-12-02 00:19:48 -0800

    refactor card sizing to be uniform; update images

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,13 +2,13 @@ import ProjectData from "/src/assets/projects.json";
 import ProjectCard from '../components/ProjectCard.jsx'
 
 export default function ProjectPage() {
-    const projects = ProjectData.map((project, index) => <li key={index}><ProjectCard project={project} /></li> )
+    const projects = ProjectData.map((project, index) => <li key={index} className="col-md-3 m-3"><ProjectCard project={project} /></li> )
 
     return (
-        <section>
+        <section className="w-100">
             <h2 className="text-center">Here&apos;s a list of projects I&apos;m proud of!</h2>
 
-            <ul className="list-unstyled">{projects}</ul>
+            <ul className="list-unstyled row d-flex justify-content-center">{projects}</ul>
         </section>
     )
 }
