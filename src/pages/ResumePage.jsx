465744bfd6bfccc447ff21abd3c45f115fdbc8b4
Nathan Geronimo <nathangero@yahoo.com>
2023-12-01 21:55:11 -0800

    implement showing skills

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,7 +1,43 @@
 
 
 export default function ResumePage() {
-    
+
+
+    const skills = {
+        frontend: [
+            "ReactJS",
+            "CSS",
+            "Bootstrap",
+            "SwiftUI"
+        ],
+        backend: [
+            "Express",
+            "MySQL",
+            "Sequelize (ORM)",
+            "NoSQL",
+            "MongoDB",
+            "Mongoose (ORM)",
+        ],
+        misc: [
+            "MERN",
+            "MVVM",
+            "MVC",
+            "Google Firebase",
+            "Xcode",
+        ]
+    }
+
+
+    const frontendItems = skills.frontend.map((skill, index) => {
+        return <li key={index}>{skill}</li>
+    })
+    const backendItems = skills.backend.map((skill, index) => {
+        return <li key={index}>{skill}</li>
+    })
+    const miscItems = skills.misc.map((skill, index) => {
+        return <li key={index}>{skill}</li>
+    })
+
 
     const onClickDownloadResume = () => {
         console.log("@onClickDownloadResume")
@@ -17,10 +53,32 @@ export default function ResumePage() {
     };
 
     return (
-        <div>
-            <button className="btn btn-primary" type="button" onClick={ onClickDownloadResume }>Download My Resume</button>
+        <section className="m-3">
+            <h2>Below you&apos;ll find a download link to my resume and a list of my skills</h2>
+            <div className="text-center">
+                <button className="btn btn-primary" type="button" onClick={onClickDownloadResume}>Download My Resume</button>
+            </div>
 
-            list of skills
-        </div>
+            <br /><br />
+
+            <div className="text-center">
+                <h3 className="text-decoration-underline fs-2">Skills</h3>
+            </div>
+
+            <h4>Front-End:</h4>
+            <ul>
+                {frontendItems}
+            </ul>
+
+            <h4>Back-End:</h4>
+            <ul>
+                {backendItems}
+            </ul>
+            
+            <h4>Misc:</h4>
+            <ul>
+                {miscItems}
+            </ul>
+        </section>
     )
 }
