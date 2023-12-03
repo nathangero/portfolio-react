788bcd1c430e37d136eb80099e40e21ebb4bc6f3
Nathan Geronimo <nathangero@yahoo.com>
2023-12-02 17:06:03 -0800

    implement notifying the user if they're missing info in the contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,18 @@
 import { Modal } from "bootstrap";
+import { useState } from "react";
 import { useMediaQuery } from 'react-responsive'
 
 export default function ContactPage() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const FIELD = {
+        NAME: "name",
+        EMAIL: "email",
+        MESSAGE: "message"
+    }
 
     const styles = {
         textarea: {
@@ -14,8 +25,12 @@ export default function ContactPage() {
             width: "50%"
         },
         button: {
-            "backgroundColor": "#2baeff",
-            "color": "white"
+            backgroundColor: "#2baeff",
+            color: "white"
+        },
+        required: {
+            "visibility": "hidden",
+            color: "red"
         }
     }
 
@@ -31,8 +46,45 @@ export default function ContactPage() {
         bootstrapModal.show();
     }
 
+    const handleOnChange = ({ target }) => {
+        console.log("@handleOnChange")
+        console.log("looking at:", target.id);
+        switch (target.id) {
+            case `contact-${FIELD.NAME}`:
+                setName(target.value)
+                break;
+
+            case `contact-${FIELD.EMAIL}`:
+                setEmail(target.value)
+                break;
+
+            case `contact-${FIELD.MESSAGE}`:
+                setMessage(target.value)
+                break;
+
+        }
+    }
+
     const handleOnBlur = ({ target }) => {
-        target.reportValidity();
+        console.log("@handleOnBlur")
+        console.log("looking at:", target.id);
+        switch (target.id) {
+            case `contact-${FIELD.NAME}`:
+                document.getElementById(`field-${FIELD.NAME}`)
+                    .style.visibility = target.checkValidity() ? "hidden" : "visible"
+                break;
+
+            case `contact-${FIELD.EMAIL}`:
+                document.getElementById(`field-${FIELD.EMAIL}`)
+                    .style.visibility = target.checkValidity() ? "hidden" : "visible"
+                break;
+
+            case `contact-${FIELD.MESSAGE}`:
+                document.getElementById(`field-${FIELD.MESSAGE}`)
+                    .style.visibility = target.checkValidity() ? "hidden" : "visible"
+                break;
+
+        }
     }
 
     const largeScreen = useMediaQuery({ query: '(min-width: 900px)' })
@@ -49,15 +101,45 @@ export default function ContactPage() {
                 <form className="" onSubmit={onClickSubmit} style={largeScreen ? styles.desktop : styles.mobile}>
                     <div className="mb-3">
                         <label htmlFor="contact-name" className="form-label">Your name</label>
-                        <input id="contact-name" type="text" className="form-control" onBlur={handleOnBlur} required />
+                        <input
+                            id="contact-name"
+                            type="text"
+                            className="form-control"
+                            onChange={handleOnChange}
+                            onBlur={handleOnBlur}
+                            required
+                        />
+                        <p id="field-name" style={styles.required}>*Please fill out your {FIELD.NAME}</p>
                     </div>
+
                     <div className="mb-3">
                         <label htmlFor="contact-email" className="form-label">Your email</label>
-                        <input id="contact-email" type="email" className="form-control" aria-describedby="emailHelp" onBlur={handleOnBlur} required />
+                        <input
+                            id="contact-email"
+                            type="email"
+                            className="form-control"
+                            aria-describedby="emailHelp"
+                            onBlur={handleOnBlur}
+                            onChange={handleOnChange}
+                            required
+                        />
+                        <p id="field-email" style={styles.required}>*Please use a valid {FIELD.EMAIL}</p>
                     </div>
+
                     <div className="mb-3">
                         <label htmlFor="contact-message" className="form-label">Message</label>
-                        <textarea id="contact-message" type="text" className="form-control" style={styles.textarea} rows={5} aria-describedby="required" onBlur={handleOnBlur} required />
+                        <textarea
+                            id="contact-message"
+                            type="text"
+                            className="form-control"
+                            style={styles.textarea}
+                            rows={5}
+                            aria-describedby="required"
+                            onBlur={handleOnBlur}
+                            onChange={handleOnChange}
+                            required
+                        />
+                        <p id="field-message" style={styles.required}>*Please fill out your {FIELD.MESSAGE}</p>
                     </div>
 
                     <div className="text-center">
