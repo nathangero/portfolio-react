69a3c74211e902adefb9194b0283a1ed5241e363
Nathan Geronimo <nathangero@yahoo.com>
2024-08-14 16:29:18 -0700

    configure App.tsx to use router

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
 import Home from "./pages/Home.tsx";
 import Error from "./pages/Error.tsx";
 import Resume from "./pages/Resume.tsx";
@@ -10,9 +11,13 @@ import Resume from "./pages/Resume.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <App />,
     errorElement: <Error />,
     children: [
+      {
+        index: true,
+        element: <Home />
+      },
       {
         path: "/resume",
         element: <Resume />
