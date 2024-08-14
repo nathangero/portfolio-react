6db79b36987bb1f07d3154da4064e28fa4fa227f
Nathan Geronimo <nathangero@yahoo.com>
2024-08-14 16:07:46 -0700

    create router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import Home from "./pages/Home.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Home />
+    //   }
+    // ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+
+export default function ErrorPage() {
+
+  return (
+    <>
+      <h1>It seems you got lost!</h1>
+    </>
+  )
+}
