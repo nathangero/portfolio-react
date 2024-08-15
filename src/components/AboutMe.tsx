9abf6714567bdb7fe938b14be2ce6344199d0674
Nathan Geronimo <nathangero@yahoo.com>
2024-08-15 11:03:05 -0700

    add profile image

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,18 @@
+
+export default function AboutMe() {
+
+  return (
+    <>
+      <img
+        id="profile-picture"
+        src="/images/selfie-aug23.jpg"
+        className="rounded-full h-48 w-48 mx-auto object-cover object-center p-2"
+        alt="nathan's face profile picture"
+      />
+
+      <h1 className="text-center text-4xl">Nathan Geronimo | Software Engineer</h1>
+
+
+    </>
+  )
+}
