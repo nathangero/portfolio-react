58dc0f7a8283dfe814603fc42290b27375c95acc
Nathan Geronimo <nathangero@yahoo.com>
2024-08-15 15:10:31 -0700

    setup initial grid

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,15 @@ export default function Projects() {
   return (
     <>
       <h2 className="text-4xl max-md:text-3xl text-center">A list of projects I'm proud of!</h2>
+
+      <div className="grid grid-cols-3 gap-10">
+        <p className="col-span-1">project 1project 1project 1project 1</p>
+        <p className="col-span-1">project 2project 2project 2project 2</p>
+        <p className="col-span-1">project 3project 3project 3project 3</p>
+        <p className="col-span-1">project 4project 4project 4project 4</p>
+        <p className="col-span-1">project 5project 5project 5project 5</p>
+        <p className="col-span-1">project 6project 6project 6project 6</p>
+      </div>
     </>
   )
 }
