854354fecc29896829987b51f1dd411038d599cf
Nathan Geronimo <nathangero@yahoo.com>
2024-08-14 19:16:28 -0700

    refactor brand to redirect to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,13 +41,19 @@ export default function Header() {
     <>
       <nav className="sticky top-0 bg-blue-500 p-4 shadow-md z-10">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white text-xl font-bold">Nathan Geronimo</div>
+          <div className="text-white text-2xl font-bold">
+            <NavLink
+              to={"/"}
+            >
+              Nathan Geronimo
+            </NavLink></div>
+
           <div className="hidden md:flex space-x-4">
-            <button onClick={() => scrollToSection('about-me')} className="text-white hover:text-gray-300">About</button>
-            <button onClick={() => scrollToSection('projects')} className="text-white hover:text-gray-300">Projects</button>
+            <button onClick={() => scrollToSection('about-me')} className="text-white text-xl hover:text-gray-300">About</button>
+            <button onClick={() => scrollToSection('projects')} className="text-white text-xl hover:text-gray-300">Projects</button>
             <NavLink
               to={"/resume"}
-              className="text-white hover:text-gray-300"
+              className="text-white text-xl hover:text-gray-300"
             >
               Resume
             </NavLink>
@@ -62,11 +68,11 @@ export default function Header() {
         </div>
         {isOpen && (
           <div className="md:hidden mt-2 space-y-2 flex flex-col items-center">
-            <button onClick={() => scrollToSection('about-me')} className="block text-white hover:text-gray-300">About</button>
-            <button onClick={() => scrollToSection('projects')} className="block text-white hover:text-gray-300">Projects</button>
+            <button onClick={() => scrollToSection('about-me')} className="block text-white text-xl hover:text-gray-300">About</button>
+            <button onClick={() => scrollToSection('projects')} className="block text-white text-xl hover:text-gray-300">Projects</button>
             <NavLink
               to={"/resume"}
-              className="block text-white hover:text-gray-300"
+              className="block text-white text-xl hover:text-gray-300"
               onClick={() => setIsOpen(false)}
             >
               Resume
