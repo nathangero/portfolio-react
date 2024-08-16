dc0fa3c6e866bd289b8c6370696fd08add297d8c
Nathan Geronimo <nathangero@yahoo.com>
2024-08-15 17:34:48 -0700

    add logo, sitemap, robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://nathan-geronimo-porfolio.netlify.app/',
+      outDir: 'dist',
+    })
+  ],
   server: {
     port: 3000,
     open: true,
